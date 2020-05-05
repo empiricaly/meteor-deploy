81e3693cd0592b1359cc3ae454b697c909943655
Garik Suess <garik@candis.io>
2020-05-05 14:26:24 +0100

    Simplifies type definition for Jsonable
    This change is expected to not have any further side-effects.

--- src/utils/jsonable.ts
+++ src/utils/jsonable.ts
@@ -1,10 +1,7 @@
 // undefined is still json'able as any keys set to undefined are truncated.
 export type JsonablePrimitive = string | number | boolean | null | undefined;
 
-export type JsonableArray =
-  | JsonableObj[]
-  | JsonablePrimitive[]
-  | JsonableArray[];
+export type JsonableArray = Jsonable[];
 
 export interface JsonableObj {
   [key: string]: JsonableObj | JsonablePrimitive | JsonableArray;
