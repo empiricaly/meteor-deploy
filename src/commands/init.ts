7d562920d13ed70292bf48c4ac55fab2d942d52a
Garik Suess <garik@candis.io>
2020-05-06 15:51:02 +0100

    Adds option to select a custom name for the pulumi project.

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -4,8 +4,10 @@ import { PulumiProjectInitializer, ExecutionReporter } from "/src/initializers";
 import { requireNoFailures } from "/src/initializers/utils";
 
 export default function (program: Command): void {
-  program.arguments("[directory]");
-  program.description("Initialize a deployment project");
+  program.arguments("[projectName]");
+  program.description(
+    "Initialize a deployment project in the current directory"
+  );
 
   const getOptions = commonOptions(
     program,
@@ -19,12 +21,12 @@ export default function (program: Command): void {
     "The description of the deployment project."
   );
 
-  program.action(async (deploymentDirectory?: string) => {
+  program.action(async (projectName?: string) => {
     const { description } = program.opts();
     const { meteorDirectory, simulation, developmentMode } = getOptions();
 
     const initializer = PulumiProjectInitializer.create(simulation).addProject({
-      deploymentDirectory,
+      projectName,
       meteorDirectory,
       description,
       developmentMode,
